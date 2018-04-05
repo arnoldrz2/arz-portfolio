7fef006cfff3c744cc566a1e210a8c7606009207
arnoldrz2 <arnoldrz2@gmail.com>
2018-04-04 20:54:49 -0500

    basic desktop optimized version of the site complete

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,7 +1,9 @@
+// Image slider function starter
 $(document).ready(function(){
     $('.slider').slider();
   });
 
+// Show more/Show less function for about me section
 if ('querySelector' in document && 
 	'addEventListener' in window) {
 
@@ -39,3 +41,46 @@ if ('querySelector' in document &&
 		});
 	});
 }
+
+// Scroll Smoothing Function
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+// Email Text Area
+$(document).ready(function() {
+    $('textarea#body').characterCounter();
+  });
